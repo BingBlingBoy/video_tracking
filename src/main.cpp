555e9a88e33928564d6e94a0edd7f37704fd1fa1
BingBlingBoy <ckeiranbarnes@gmail.com>
2024-09-01 12:01:00 +0100

    Inital Commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <opencv4/opencv2/core/hal/interface.h>
+#include <opencv4/opencv2/highgui.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+
+}
